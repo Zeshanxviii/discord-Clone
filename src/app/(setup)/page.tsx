c2fd9382e85dff4198dccfa6fb8eb460c19ee0ce
Zeeshan <mzh881061@gmail.com>
2025-04-11 20:00:22 +0530

    initail ui

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,7 +1,28 @@
+import { redirect } from "next/navigation";
+import initialProfile from "../../../libs/initial-profile"
+import { db } from "../../../libs/db";
+import { InitialModel } from "@/components/models/initial-model";
+
 const SetupPage = async() => {
+    const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where:{
+            member: {
+                some: {
+                    profileId: profile.userId
+                }
+            }  
+        }
+    });
+
+    if(server)
+    {
+        return redirect(`/servers/${server.id}`);
+    }
     return (  
         <div>
-            Create a Server
+            <InitialModel/>
         </div>
     );
 }
